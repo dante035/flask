26b52401ce3850024600b04b8604413b58c53196
dante035 <105222598+dante035@users.noreply.github.com>
2022-05-09 19:37:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+@app.route('/About')
+def About():
+    return render_template('About.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/service')
+def service():
+    return render_template('service.html')
+app.run(debug=True)
